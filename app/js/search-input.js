398ae4f5616a6b57aa48de57a7b427e035635bca
Alina To <alina.to@gmail.com>
2015-10-17 12:28:53 -0700

    Build out index.html

---
+++ app/js/search-input.js
@@ -0,0 +1,6 @@
+$('.searchbox-input').hide();
+
+$('.icon-search').on('click', function() {
+  $('.searchbox-input').animate({width:'toggle'},800);
+});
+
--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -7,7 +7,6 @@ gulp.task('watch', function() {
   gulp.watch(config.index.src, ['index']);
   gulp.watch(config.html.src, ['html']);
 
-  gulp.watch(['./node_modules/angular/angular.js', './node_modules/angular-route/angular-route.js']);
   // Watchify will watch and recompile our JS, so no need to gulp.watch it
 });
 
