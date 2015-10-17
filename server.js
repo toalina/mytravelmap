05bd2b11effb18ecfde4ef96e30aa85b3036cbab
Phil <philson3@gmail.com>
2015-10-16 17:21:26 -0700

    staging for models

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+var mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URL || 'mongodb://localhost/travel_map');
+process.env.APP_SECRET = process.env.APP_SECRET || 'crazyawesome';
+
+app.use(express.static(__dirname + '/build'));
+
+var userRouter = require(__dirname + '/routes/user_routes');
+app.use('/api', userRouter);
+
+
+var port = process.env.PORT || 3000;
+app.listen(port, function (){
+  console.log('the server is running at port ' + port);
+});
+
