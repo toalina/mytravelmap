10670519aa9c3d460345f554ac0d63b2f42b09f8
Alina To <alina.to@gmail.com>
2015-10-17 21:20:28 -0700

    Add working responsive nav-main

--- app/js/responsive-nav.js
+++ app/js/responsive-nav.js
@@ -1,34 +1,9 @@
-var menuLeft = document.getElementById( 'nav-main-s1' ),
-
-    showLeft = document.getElementById( 'showLeft' ),
-
+var menuRight = document.getElementById( 'nav-main-s2' ),
+    showRight = document.getElementById( 'showRight' ),
     body = document.body;
 
-$('#showLeft').on('click', function() {
+$('#showRight').on('click', function() {
   classie.toggle( this, 'active' );
-  classie.toggle( menuLeft, 'nav-main-open' );
-  disableOther( 'showLeft' );
+  classie.toggle( menuRight, 'nav-main-open' );
 });
 
-
-
-// function disableOther( button ) {
-//   if( button !== 'showLeft' ) {
-//     classie.toggle( showLeft, 'disabled' );
-//   }
-//   if( button !== 'showRight' ) {
-//     classie.toggle( showRight, 'disabled' );
-//   }
-//   if( button !== 'showTop' ) {
-//     classie.toggle( showTop, 'disabled' );
-//   }
-//   if( button !== 'showBottom' ) {
-//     classie.toggle( showBottom, 'disabled' );
-//   }
-//   if( button !== 'showLeftPush' ) {
-//     classie.toggle( showLeftPush, 'disabled' );
-//   }
-//   if( button !== 'showRightPush' ) {
-//     classie.toggle( showRightPush, 'disabled' );
-//   }
-// }
