749fda444232fe2ae91b83b9ca000a7e2aad5cf5
Alina To <alina.to@gmail.com>
2015-10-17 22:58:35 -0700

    Add working carousel in banner component

--- app/js/entry.js
+++ app/js/entry.js
@@ -2,3 +2,8 @@ require('./search-input.js');
 require('./classie.js');
 require('./responsive-nav.js');
 
+// require('./slick/slick.js');
+require('./slick/slick-app.js');
+require('./slick/jquery.js');
+
+
---
+++ app/js/slick/slick-app.js
@@ -0,0 +1,9 @@
+'use sctrict';
+$(document).ready(function(){
+  $('.landing-carousel').slick({
+    dots: true,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 5000
+  });
+});
