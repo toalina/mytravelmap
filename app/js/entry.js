dba601595ec345b79069d7e14764f858481acb0f
Alina To <alina.to@gmail.com>
2015-10-19 00:07:54 -0700

    Add background-images to sections

--- app/js/entry.js
+++ app/js/entry.js
@@ -5,5 +5,6 @@ require('./responsive-nav.js');
 // require('./slick/slick.js');
 require('./slick/slick-app.js');
 // require('./slick/jquery.js');
-
+require('./scroll-up-bar.js');
+require('./scrollup-app.js');
 
