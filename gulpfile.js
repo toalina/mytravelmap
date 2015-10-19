f79d6f7c4f337d3df63640ce9c54e0fd79a7bf3a
Alina To <alina.to@gmail.com>
2015-10-19 13:08:38 -0700

    Reconfigure gulpfile.js and /gulp/tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,22 @@
-var gulp = require('gulp');
-var mocha = require('gulp-mocha');
-var webpack = require('webpack-stream');
+// var gulp = require('gulp');
+// var mocha = require('gulp-mocha');
+// var webpack = require('webpack-stream');
 
-gulp.task('webpack:dev', function() {
-  return gulp.src('./app/js/app.js') 
-    .pipe(webpack({
-      output: { filename: 'bundle.js'}
-    }))
-    .pipe(gulp.dest('build/'));
-}); //webpack for the development
+// gulp.task('webpack:dev', function() {
+//   return gulp.src('./app/js/app.js')
+//     .pipe(webpack({
+//       output: { filename: 'bundle.js'}
+//     }))
+//     .pipe(gulp.dest('build/'));
+// }); //webpack for the development
 
-gulp.task('staticfiles:dev', function() {
-  return gulp.src('./app/**/*.html')
-    .pipe(gulp.dest('build'))
-});
+// gulp.task('staticfiles:dev', function() {
+//   return gulp.src('./app/**/*.html')
+//     .pipe(gulp.dest('build'))
+// });
 
-gulp.task('default', ['staticfiles:dev','webpack:dev']);
+
+var requireDir = require('require-dir');
+
+// Require all tasks in gulp/tasks, including subfolders
+requireDir('./gulp/tasks', { recurse: true });
