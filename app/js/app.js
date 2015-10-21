bafaa8caf52f3b75a4539015a9772bdc1e235714
Alina To <alina.to@gmail.com>
2015-10-20 23:54:26 -0700

    Add angular-animate and css hover effect

--- app/js/app.js
+++ app/js/app.js
@@ -1,8 +1,9 @@
 require('angular/angular');
 var angular = window.angular;
 require('angular-route');
+require('angular-animate');
 
-var mapsApp = angular.module('mapsApp', ['ngRoute']);
+var mapsApp = angular.module('mapsApp', ['ngRoute', 'ngAnimate']);
 
 require('./controllers/controllers')(mapsApp);
 require('./services/services')(mapsApp);
---
+++ app/js/styles/fade-in.js
@@ -0,0 +1,5 @@
+'use strict';
+
+// dashboard links
+$('.fade-in').hide(0).delay(1000).fadeIn(3000);
+
