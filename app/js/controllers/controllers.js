06adbd6a781e2ab9e2b44f2074d3cb662922ce8e
Alina To <alina.to@gmail.com>
2015-10-22 13:56:37 -0700

    Resolve merge conflicts

--- app/js/controllers/controllers.js
+++ app/js/controllers/controllers.js
@@ -3,7 +3,6 @@ module.exports = function(app) {
   require('./signin_controller.js')(app);
   require('./signup_controller.js')(app);
   require('./photo_controller.js')(app);
-  require('./dashboard_controller.js')(app);
   require('./trips_controller.js')(app);
   require('./plan_controller')(app);
 };
