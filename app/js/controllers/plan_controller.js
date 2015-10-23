1755537012c4003f0c1ac9130deb6d5ce0982d3e
Jeff Gerber <jallengerber@gmail.com>
2015-10-22 18:18:25 -0700

    added angular to gallery

--- app/js/controllers/plan_controller.js
+++ app/js/controllers/plan_controller.js
@@ -1,10 +1,10 @@
 module.exports = function(app){
   app.controller('PlanController', ['$rootScope', '$scope', '$http', '$location', '$cookies', function($rootScope, $scope, $http, $location, $cookies){
-    
+
     var eat = $cookies.get('eat');
     if (!(eat && eat.length))
       $location.path('/signup');
-    
+
     $http.defaults.headers.common.token = eat;
 
     $scope.test = function(){
@@ -39,8 +39,18 @@ module.exports = function(app){
         function(res){console.log('Update Successful!!!')},
         function(res){console.log('Failed to update record')}
       )
-    }
+    };
 
+    $scope.photo0 = "https://res-4.cloudinary.com/dhtizamqh/image/upload/t_media_lib_thumb/v1445453242/q9l0fccwgmei03pywurd.jpg";
+    $scope.photo1 = "https://res-4.cloudinary.com/dhtizamqh/image/upload/t_media_lib_thumb/v1445453242/q9l0fccwgmei03pywurd.jpg";
+    $scope.photo2 = "https://res-4.cloudinary.com/dhtizamqh/image/upload/t_media_lib_thumb/v1445453242/q9l0fccwgmei03pywurd.jpg";
+    $scope.photo3 = "https://res-4.cloudinary.com/dhtizamqh/image/upload/t_media_lib_thumb/v1445469204/lpyfude00vt1ihsvlyej.jpg";
+    $scope.photo4 = "https://res-4.cloudinary.com/dhtizamqh/image/upload/t_media_lib_thumb/v1445453242/q9l0fccwgmei03pywurd.jpg";
+    $scope.photo5 = "https://res-4.cloudinary.com/dhtizamqh/image/upload/t_media_lib_thumb/v1445453242/q9l0fccwgmei03pywurd.jpg";
+
+    $scope.photoPopulater = function(){
+
+    };
 
 
 
